97a7f78e31c37ff06cf28d258c3a65247ac1777c
ShwetaKamble23 <107830011+ShwetaKamble23@users.noreply.github.com>
2022-07-01 10:32:58 +0530

    Add files via upload

---
+++ ArithmaticDemo.java
@@ -0,0 +1,39 @@
+ public class ArithmaticDemo 
+{
+	public void add()
+	{
+		int a= 20;
+		int b = 10;
+		int res = a + b;
+		System.out.println("Addition : "+res);
+	}
+	public void sub()
+	{
+		int a= 20;
+		int b = 10;
+		int res = a - b;
+		System.out.println("Subtraction : "+res);
+	}
+	public void mul()
+	{
+		int a= 20;
+		int b = 10;
+		int res = a * b;
+		System.out.println("Multiplication : "+res);
+	}
+	public void div()
+	{
+		int a= 20;
+		int b = 10;
+		int res = a / b;
+		System.out.println("Division : "+res);
+	}
+	public static void main(String[] args)
+	{
+		ArithmaticDemo d = new ArithmaticDemo();
+		d.add();
+		d.sub();
+		d.mul();
+		d.div();
+	}
+}
